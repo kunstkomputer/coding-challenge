12e51eb0dc7a503dad15892a991b41b310dca9ac
Jonas Pruditsch <6641029+kunstkomputer@users.noreply.github.com>
2021-09-07 16:16:24 +0200

    parse articles from mock .csv

--- product_list/app.py
+++ product_list/app.py
@@ -1,9 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+import csv
+from decimal import *
 
-def read_csv(num1, num2):
-    return num1 + num2
+
+
+class Article:
+    def __init__(self, article_id, prod_id, name, description, price, amount):
+        self.article_id = article_id
+        self.prod_id = prod_id
+        self.name = name
+        self.description = description
+        self.price = price
+        self.amount = amount
+
+    def __eq__(self, other):
+        if not isinstance(self, other.__class__):
+            return False
+
+        return self.article_id == other.article_id and self.prod_id == other.prod_id and \
+            self.name == other.name and self.description == other.description and \
+            self.price == other.price and self.amount == other.amount
+
+
+def read_articles_from_csv(csv_file_path):
+    data = list(csv.reader(open(csv_file_path), delimiter='|', lineterminator='\n'))
+    print(data)
+    article_instances = []
+    for row in data[1:]:
+        article_instances.append(
+            Article(row[0], row[1], row[2], row[3], Decimal(row[4]), int(row[5])))
+
+    return article_instances
 
 
 if __name__ == '__main__':
-    read_csv()
+    read_articles_from_csv('../tests/data/a.csv')
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+from decimal import Decimal
+
+import pytest
 
 from product_list import app
 
 
-def test_read_csv():
-    assert app.read_csv(1, 2) == 3
+@pytest.fixture
+def mock_csv_data():
+    return [
+        "id|produktId|name|beschreibung|preis|bestand",
+        "A-cVBTQHVF|P-cVBTQHVF|OBLAEDD|Gfaokn Ttefoa pfrnZ|58.77|38",
+        "A-PnZZzJFO|P-cVBTQHVF|EDDBZM|fdlViTtefo fpfrnZj hva czIsmugweh|8.35|39"
+    ]
+
+
+@pytest.fixture
+def mock_csv_file(tmp_path, mock_csv_data):
+    datafile = tmp_path / "articles.csv"
+    datafile.write_text("\n".join(mock_csv_data))
+    return str(datafile)
+
+
+def test_read_articles_from_csv(mock_csv_file):
+    mock_instances = [
+        app.Article("A-cVBTQHVF", "P-cVBTQHVF", "OBLAEDD", "Gfaokn Ttefoa pfrnZ", Decimal('58.77'),
+                    38),
+        app.Article("A-PnZZzJFO", "P-cVBTQHVF", "EDDBZM", "fdlViTtefo fpfrnZj hva czIsmugweh",
+                    Decimal('8.35'), 39),
+
+    ]
+    assert app.read_articles_from_csv(mock_csv_file) == mock_instances
