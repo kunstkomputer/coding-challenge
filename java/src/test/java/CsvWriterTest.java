63e1d06de351538e864d681a43f3686d711e036f
Jonas Pruditsch <6641029+kunstkomputer@users.noreply.github.com>
2021-09-11 04:19:04 +0200

    minor: fixes formatting of test file

--- java/src/test/java/CsvWriterTest.java
+++ java/src/test/java/CsvWriterTest.java
@@ -21,11 +21,11 @@ public class CsvWriterTest {
     @Test
     public void writeProductListToCsv() throws IOException {
         List<Product> prodList = new ArrayList<>();
-        prodList.add(new Product("P-SA6YNXRd","KCBOXLLTPM FFSQ","xs lzul n lmpPPf mjd",29.85f,27));
-        prodList.add(new Product("P-A6YNXRdD","CVKCBOX","",29.85f,27));
-        prodList.add(new Product("P-SYNXRdDY","KCBOXLLTPM FFSQ","xs lzul n lmpPPf mjd",29.85f,27));
-        prodList.add(new Product("P-6YNXRdDY","KCBOXLLTPM FFSQ","xs lzul n lmpPPf mjd",29.85f,134));
-        prodList.add(new Product("P-XRdDYcOL","FFSQ AOXKS","zulHklmpP",17.09f,48));
+        prodList.add(new Product("P-SA6YNXRd", "KCBOXLLTPM FFSQ", "xs lzul n lmpPPf mjd", 29.85f, 27));
+        prodList.add(new Product("P-A6YNXRdD", "CVKCBOX", "", 29.85f, 27));
+        prodList.add(new Product("P-SYNXRdDY", "KCBOXLLTPM FFSQ", "xs lzul n lmpPPf mjd", 29.85f, 27));
+        prodList.add(new Product("P-6YNXRdDY", "KCBOXLLTPM FFSQ", "xs lzul n lmpPPf mjd", 29.85f, 134));
+        prodList.add(new Product("P-XRdDYcOL", "FFSQ AOXKS", "zulHklmpP", 17.09f, 48));
 
 
         final File tempFile = tempFolder.newFile();
@@ -35,15 +35,15 @@ public class CsvWriterTest {
         String premadeCsvTestFileContent = new String(getClass().getClassLoader().getResourceAsStream("sampleProductList.csv").readAllBytes());
 
         final String generatedCsvTestFileContent = Files.readString(Path.of(tempFile.getAbsolutePath()));
-        assertEquals(premadeCsvTestFileContent,generatedCsvTestFileContent);
+        assertEquals(premadeCsvTestFileContent, generatedCsvTestFileContent);
     }
 
     @Test
     public void floatsAreRenderedAsTwoDigitsAfterDecimalPoint() throws IOException {
         List<Product> prodList = new ArrayList<>();
 
-        Product bothPriceDigitsZero = new Product("P-SA6YNXRd","KCBOXLLTPM FFSQ","xs lzul n lmpPPf mjd",29.00f,27);
-        Product lastPriceDigitsZero = new Product("P-A6YNXRdD","CVKCBOX","",29.50f,27);
+        Product bothPriceDigitsZero = new Product("P-SA6YNXRd", "KCBOXLLTPM FFSQ", "xs lzul n lmpPPf mjd", 29.00f, 27);
+        Product lastPriceDigitsZero = new Product("P-A6YNXRdD", "CVKCBOX", "", 29.50f, 27);
 
         prodList.add(bothPriceDigitsZero);
         prodList.add(lastPriceDigitsZero);
@@ -55,7 +55,7 @@ public class CsvWriterTest {
         String premadeCsvTestFileContent = new String(getClass().getClassLoader().getResourceAsStream("sampleProductListWithFloatFormat.csv").readAllBytes());
 
         final String generatedCsvTestFileContent = Files.readString(Path.of(tempFile.getAbsolutePath()));
-        assertEquals(premadeCsvTestFileContent,generatedCsvTestFileContent);
+        assertEquals(premadeCsvTestFileContent, generatedCsvTestFileContent);
     }
 
 }
